a6479ee4a9853c593efe392c5debf9b6e00c9486
Nitin <nitin.e.singh@capgemini.com>
2021-06-01 22:51:32 +0530

    Day 09 - Recursion

---
+++ Day_09/Solution.py
@@ -0,0 +1,22 @@
+'''
+Author: Nitin Singh
+Email : nitin7570@gmail.com
+
+Sample Input:
+3
+
+Sample Output:
+6
+
+'''
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+if __name__ == '__main__':
+    n = int(input())
+    print(factorial(n))
+    

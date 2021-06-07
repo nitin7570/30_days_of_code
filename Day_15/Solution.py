593d1fc94fd11c4b584285079fba6e06bbd809b9
Nitin <nitin.e.singh@capgemini.com>
2021-06-07 22:56:40 +0530

    Day 15 - Linked List

---
+++ Day_15/Solution.py
@@ -0,0 +1,47 @@
+'''
+Author: Nitin Singh
+Email : nitin7570@gmail.com
+
+Sample Input:
+STDIN   Function
+-----   --------
+4       T = 4
+2       first data = 2
+3
+4
+1       fourth data = 1
+
+Sample Output:
+2 3 4 1
+
+'''
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+        if head is None:
+            head = Node(data)
+            self.tail = head
+        else: 
+            node = Node(data)
+            self.tail.next = node
+            self.tail = node
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head)

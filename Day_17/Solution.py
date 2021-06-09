b188ff8de5d6dfa1c48a5e50a0954ba9bfa337d9
Nitin <nitin.e.singh@capgemini.com>
2021-06-09 23:07:20 +0530

    Day 17 - More Exceptions

---
+++ Day_17/Solution.py
@@ -0,0 +1,35 @@
+'''
+Author: Nitin Singh
+Email : nitin7570@gmail.com
+
+Sample Input:
+4
+3 5
+2 4
+-1 -2
+-1 3
+
+Sample Output:
+243
+16
+n and p should be non-negative
+n and p should be non-negative
+
+'''
+
+class Calculator:
+    def power(self, n, p):
+        if n < 0 or p < 0:
+            raise ValueError('n and p should be non-negative')
+        return n ** p
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
+        
